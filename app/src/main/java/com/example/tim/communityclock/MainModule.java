abc1eaecb0a5d5a3a0d27317dc213070878be006
Timothée Rey-Vibet <timothee.reyvibet@gmail.com>
2018-11-18 18:04:25 +0100

    di implementation

---
+++ app/src/main/java/com/example/tim/communityclock/MainModule.java
@@ -0,0 +1,11 @@
+package com.example.tim.communityclock;
+
+import android.arch.lifecycle.ViewModelProvider;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class MainModule {
+
+}
