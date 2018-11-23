ecfcb892e0acf9c43dc69264da53cca68111ba7b
Thomas Perrin <thomasperrin@MacBook-Pro-de-Thomas.local>
2018-11-23 17:29:07 +0100

    add message repo

--- app/src/main/java/com/example/tim/communityclock/domain/message/repository/MessageRepository.kt
+++ app/src/main/java/com/example/tim/communityclock/domain/message/repository/MessageRepository.kt
@@ -1,13 +1,4 @@
 package com.example.tim.communityclock.domain.message.repository
 
-import com.example.tim.communityclock.data.model.api.Message
-import io.reactivex.Completable
-import io.reactivex.Observable
-
-interface MessagesRepository {
-
-    fun getOneMessage(): Observable<Message>
-
-    fun sendMessage(message: Message): Completable
-
+interface MessageRepository {
 }
